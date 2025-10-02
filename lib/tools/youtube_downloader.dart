7eb04a29ce42ba6e02bb7918e581ecfad65362ac
Makdum Ibrohim <makdumibrohim3@gmail.com>
2025-10-02 11:07:18 +0700

    initial commit

---
+++ lib/tools/youtube_downloader.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class YoutubeDownloader extends StatelessWidget {
+  const YoutubeDownloader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
